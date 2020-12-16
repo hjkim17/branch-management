0267360cf8da76a289b036e7d17c9b4d88f1e4bd
hjkim17 <ouiqdmw17@gmail.com>
2020-12-16 16:59:54 +0900

    Change activate_crontab.sh to make job log

--- activate_crontab.sh
+++ activate_crontab.sh
@@ -1,5 +1,5 @@
 crontab -l > branch_mgmt_cron
-manager_loc="$(pwd)/branch_manager.py"
-echo "0 20 * * * python $manager_loc" >> branch_mgmt_cron
+manager_loc="$(pwd)"
+echo "0 20 * * * * python $manager_loc/branch_manager.py >> $manager_loc/mgmt.log" >> branch_mgmt_cron
 crontab branch_mgmt_cron
 rm branch_mgmt_cron
